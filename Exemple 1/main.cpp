c0d9ad585a0bfafa909cec683c513226ec8aec92
Fo170 <65036208+Fo170@users.noreply.github.com>
2025-12-23 17:17:06 +0100

    Create main.cpp
    Exemple 1 : Surveillance simple

---
+++ Exemple 1/main.cpp
@@ -0,0 +1,16 @@
+// Exemple 1 : Surveillance simple
+#include <MemoryInfo.h>
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Alloue de la mémoire pour le test
+  char* buffer = (char*)malloc(1024);
+  
+  // Affiche les informations
+  MemoryInfo.display();
+  
+  free(buffer);
+}
+
+void loop() {}
